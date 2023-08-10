0680aafa6261ed33fa03b950282e7864e55d5f6b
badoodee95 <monzoncaleb@yahoo.com>
2023-08-09 19:57:44 -0700

    Created signup page

--- src/app/users/signup/page.js
+++ src/app/users/signup/page.js
@@ -123,92 +123,66 @@ const Signup = () => {
 	// You can have them redirected to profile (your choice)
 
 	return (
-		<div className="container">
-			<br />
-			<br />
-			<br />
-			<br />
-			<div className="row justify-content-center">
-				<div className="col-md-8">
-					<div className="card-group mb-0">
-						<div className="card p-4">
-							<div className="card-body">
-								<form onSubmit={handleSubmit}>
-									<h1>Sign Up</h1>
-									<p className="text-muted">Create an account below to get started</p>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
-										<input type="text" className="form-control" placeholder="First Name" value={firstName} onChange={handleFirstName} required/>
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-whatsapp"></i></span>
-										<input type="text" className="form-control" placeholder="Last Name" value={lastName} onChange={handleLastName} required/>
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-mail-forward" aria-hidden="true"></i></span>
-										<input type="email" className="form-control" placeholder="Email" value={email} onChange={handleEmail} required/>
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-lock"></i></span>
-										<input type="password" className="form-control" placeholder="Password" value={password} onChange={handlePassword} required/>
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-										<input type="text" className="form-control" placeholder="Job Title" value={jobTitle} onChange={handleJobTitle} />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-										<input type="text" className="form-control" placeholder="Phone Number" value={number} onChange={handleNumber} />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-										<input type="text" className="form-control" placeholder="Street Address" value={streetAddress} onChange={handleStreetAddress} />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-										<input type="text" className="form-control" placeholder="City" value={city} onChange={handleCity} />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-										<input type="text" className="form-control" placeholder="State" value={state} onChange={handleState} />
-									</div>
-									<div className="input-group mb-3">
-										<span className="input-group-addon"><i className="fa fa-address-book"></i></span>
-										<input type="text" className="form-control" placeholder="Zip Code" value={zipCode} onChange={handleZipCode} />
-									</div>
-									<div className="row">
-										<div className="col-6">
-											<button type="submit" className="btn btn-primary px-4">Sign Up</button>
+		<section className="vh-100 bg-image"
+			style={{ backgroundImage: "url('https://ca.slack-edge.com/T0351JZQ0-U04NEAZUL3T-20e13f3e4c10-512')", backgroundSize: "cover", backgroundPosition: "center" }}
+		>
+			<div className="mask d-flex align-items-center h-100 gradient-custom-3">
+				<div className="container h-100">
+					<div className="row d-flex justify-content-center align-items-center h-100">
+						<div className="col-12 col-md-9 col-lg-7 col-xl-6">
+							<div className="card" style={{ borderRadius: "15px", backgroundColor: 'rgba(210, 91, 234, 0.5)' }}>
+								<div className="card-body p-5">
+									<h2 className="text-center mb-5 text-light">Create your Timagotchi Account</h2>
+
+									<form>
+
+										<div className="form-outline mb-4 text-light">
+											<input type="text" id="form3Example1cg" className="form-control form-control-lg" />
+											<label className="form-label" htmlFor="form3Example1cg">Email</label>
 										</div>
-										<div className="col-6 text-right">
-											<button type="button" className="btn btn-link px-0">Forgot password?</button>
+
+										<div className="form-outline mb-4 text-light">
+											<input type="email" id="form3Example3cg" className="form-control form-control-lg" />
+											<label className="form-label" htmlFor="form3Example3cg">Password</label>
 										</div>
-									</div>
-								</form>
-							</div>
-						</div>
-						<div className="card text-white bg-primary py-5 d-md-down-none" style={{ width: "44%" }}>
-							<div className="card-body text-center">
-								<br />
-								<br />
-								<br />
-								<br />
-								<br />
-								<br />
-								<br />
-								<br />
-								<div>
-									<h2>Login</h2>
-									<p>Sign In to your account</p>
-									<a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
+
+										<div className="form-outline mb-4 text-light">
+											<input type="password" id="form3Example4cg" className="form-control form-control-lg" />
+											<label className="form-label" htmlFor="form3Example4cg">First Name</label>
+										</div>
+
+										<div className="form-outline mb-4 text-light">
+											<input type="password" id="form3Example4cdg" className="form-control form-control-lg" />
+											<label className="form-label" htmlFor="form3Example4cdg">Last Name</label>
+										</div>
+
+										<div className="form-outline mb-4 text-light">
+											<input type="password" id="form3Example4cdg" className="form-control form-control-lg" />
+											<label className="form-label" htmlFor="form3Example4cdg">Location</label>
+										</div>
+
+										<div className="form-outline mb-4 text-light">
+											<input type="date" className="form-control form-control-lg" />
+											<label className="form-label" htmlFor="form3Example4cdg">Birthdate</label>
+										</div>
+
+										<div className="d-flex justify-content-center">
+											<button type="button"
+												className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+										</div>
+
+										<p className="text-center mt-5 mb-0 text-light">Already have an account? <a href="/users/login"
+											className="fw-bold text-body"><u>Login here</u></a></p>
+									</form>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</section >
 	);
+
 };
 
 export default Signup;
