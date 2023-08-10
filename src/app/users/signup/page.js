d389f00f83798125f332404c0a9717a549436b02
Jay Lee <randomdori@gmail.com>
2023-08-09 22:14:16 -0700

    feat: add signup functionality

--- src/app/users/signup/page.js
+++ src/app/users/signup/page.js
@@ -2,103 +2,67 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 const Signup = () => {
-	const router = useRouter();
-
-	const [redirect, setRedirect] = useState(false);
-
-	const [firstName, setFirstName] = useState('');
-	const [lastName, setLastName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-	const [jobTitle, setJobTitle] = useState('');
-	const [number, setNumber] = useState('');
-	const [streetAddress, setStreetAddress] = useState('');
-	const [city, setCity] = useState('');
-	const [state, setState] = useState('');
-	const [zipCode, setZipCode] = useState('');
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [location, setLocation] = useState('');
+	const [birthdate, setBirthdate] = useState('');
+
 	const [error, setError] = useState(false);
 
-	// create the 
-	const handleFirstName = (e) => {
-		// fill in code
-		setFirstName(e.target.value);
-	};
-
-	const handleLastName = (e) => {
-		// fill in code
-		setLastName(e.target.value);
-	};
+	const [redirect, setRedirect] = useState(false);
+	const router = useRouter();
 
 	const handleEmail = (e) => {
-		// fill in code
 		setEmail(e.target.value);
 	};
 
 	const handlePassword = (e) => {
-		// fill in code
 		setPassword(e.target.value);
 	};
 
-	const handleJobTitle = (e) => {
-		// fill in code
-		setJobTitle(e.target.value);
+	const handleFirstName = (e) => {
+		setFirstName(e.target.value);
 	};
 
-	const handleNumber = (e) => {
-		// fill in code
-		setNumber(e.target.value);
+	const handleLastName = (e) => {
+		setLastName(e.target.value);
 	};
 
-	const handleStreetAddress = (e) => {
-		// fill in code
-		setStreetAddress(e.target.value);
+	const handleLocation = (e) => {
+		setLocation(e.target.value);
 	};
 
-	const handleCity = (e) => {
-		// fill in code
-		setCity(e.target.value);
-	};
-
-	const handleState = (e) => {
-		// fill in code
-		setState(e.target.value);
-	};
-
-	const handleZipCode = (e) => {
-		// fill in code
-		setZipCode(e.target.value);
+	const handleBirthdate = (e) => {
+		setBirthdate(e.target.value);
 	};
 
 	const handleSubmit = (e) => {
-		e.preventDefault(); // at the beginning of a submit function
-
-		const newUser = {
-			firstName,
-			lastName,
-			email,
-			jobTitle,
-			number,
-			password,
-			streetAddress,
-			city,
-			state,
-			zipCode
-		};
+		e.preventDefault();
+		const newUser = { email, password, firstName, lastName, location, birthdate };
+		console.log(newUser);
 		axios.post(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/signup`, newUser)
 			.then(response => {
+				// console.log('response', response.data);
+				if (typeof window !== 'undefined') {
+					localStorage.setItem('email', response.data.user.email);
+					localStorage.setItem('userId', response.data.user._id);
+				}
 				setRedirect(true);
 			})
 			.catch(error => {
 				if (error.response.data.message === 'Email already exists') {
 					console.log('===> Error in Signup', error.response.data.message);
-					setError(true);
+					// setError(true);
 				}
 			});
-
 	};
+
 
 	if (redirect) { router.push('/users/login'); }
 	if (error) {
@@ -111,21 +75,19 @@ const Signup = () => {
 							<br />
 							<h2>Login</h2>
 							<p>Sign In to your account</p>
-							<a href="/users/login" type="button" className="btn btn-primary active mt-3">Login</a>
+							<Link href="/users/login" type="button" className="btn btn-primary active mt-3">Login</Link>
 							<span>  </span>
-							<a href="/users/signup" type="button" className="btn btn-secondary active mt-3">Signup</a>
+							<Link href="/users/signup" type="button" className="btn btn-secondary active mt-3">Signup</Link>
 						</div>
 					</div>
 				</div>
 			</div>
 		);
 	}
-	// You can have them redirected to profile (your choice)
 
 	return (
-		<section className="vh-100 bg-image"
-			style={{ backgroundImage: "url('https://ca.slack-edge.com/T0351JZQ0-U04NEAZUL3T-20e13f3e4c10-512')", backgroundSize: "cover", backgroundPosition: "center" }}
-		>
+		<section className="vh-100 vw-100 bg-image"
+			style={{ backgroundImage: "url('https://img.freepik.com/free-vector/frame-with-dogs-vector-white-background_53876-127700.jpg?w=2000')", backgroundSize: "cover", backgroundPosition: "center" }}>
 			<div className="mask d-flex align-items-center h-100 gradient-custom-3">
 				<div className="container h-100">
 					<div className="row d-flex justify-content-center align-items-center h-100">
@@ -134,46 +96,37 @@ const Signup = () => {
 								<div className="card-body p-5">
 									<h2 className="text-center mb-5 text-light">Create your Timagotchi Account</h2>
 
-									<form>
-
+									<form onSubmit={handleSubmit} className='text-center'>
 										<div className="form-outline mb-4 text-light">
-											<input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-											<label className="form-label" htmlFor="form3Example1cg">Email</label>
+											<label className="form-label" htmlFor="form3Example4cg">Email</label>
+											<input type="email" name="email" value={email} onChange={handleEmail} className="form-control" required />
 										</div>
-
 										<div className="form-outline mb-4 text-light">
-											<input type="email" id="form3Example3cg" className="form-control form-control-lg" />
-											<label className="form-label" htmlFor="form3Example3cg">Password</label>
+											<label className="form-label" htmlFor="form3Example4cg">Password</label>
+											<input type="password" name="password" value={password} onChange={handlePassword} className="form-control" required />
 										</div>
-
 										<div className="form-outline mb-4 text-light">
-											<input type="password" id="form3Example4cg" className="form-control form-control-lg" />
 											<label className="form-label" htmlFor="form3Example4cg">First Name</label>
+											<input type="text" name="firstName" value={firstName} onChange={handleFirstName} className="form-control" required />
 										</div>
-
 										<div className="form-outline mb-4 text-light">
-											<input type="password" id="form3Example4cdg" className="form-control form-control-lg" />
-											<label className="form-label" htmlFor="form3Example4cdg">Last Name</label>
+											<label className="form-label" htmlFor="form3Example4cg">Last Name</label>
+											<input type="text" name="lastName" value={lastName} onChange={handleLastName} className="form-control" required />
 										</div>
-
 										<div className="form-outline mb-4 text-light">
-											<input type="password" id="form3Example4cdg" className="form-control form-control-lg" />
-											<label className="form-label" htmlFor="form3Example4cdg">Location</label>
+											<label className="form-label" htmlFor="form3Example4cg"> Location</label>
+											<input type="text" name="location" value={location} onChange={handleLocation} className="form-control" />
 										</div>
-
 										<div className="form-outline mb-4 text-light">
-											<input type="date" className="form-control form-control-lg" />
-											<label className="form-label" htmlFor="form3Example4cdg">Birthdate</label>
+											<label className="form-label" htmlFor="form3Example4cg"> Birthdate</label>
+											<input type="Date" name="birthdate" value={birthdate} onChange={handleBirthdate} className="form-control" />
 										</div>
-
 										<div className="d-flex justify-content-center">
-											<button type="button"
-												className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
+											<button type="submit" className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
 										</div>
-
-										<p className="text-center mt-5 mb-0 text-light">Already have an account? <a href="/users/login"
-											className="fw-bold text-body"><u>Login here</u></a></p>
 									</form>
+									<p className="text-center mt-5 mb-0 text-light">Already have an account? <Link href="/users/login"
+										className="fw-bold text-body"><u>Login here</u></Link></p>
 								</div>
 							</div>
 						</div>
