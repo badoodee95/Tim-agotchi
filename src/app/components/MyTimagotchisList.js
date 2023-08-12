4208e685573511f55edb1f17d60fc79153fad2ac
Jay Lee <randomdori@gmail.com>
2023-08-11 20:28:58 -0700

    feat: add funtionality on mytimlist

--- src/app/components/MyTimagotchisList.js
+++ src/app/components/MyTimagotchisList.js
@@ -1,11 +1,60 @@
 'use client';
 import MyTimagotchi from "./MyTimagotchi";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import jwtDecode from "jwt-decode";
+import axios from "axios";
+import Link from "next/link";
+import { el } from "@faker-js/faker";
 
-export default function MyTimagotchiList({ Timagtochis }) {
+export default function MyTimagotchiList({ currentUserId }) {
+
+    const router = useRouter();
+    const [loggedInUser, setLoggedInUser] = useState(null);
+    const [tims, setTims] = useState([]);
+    const [redirect, setRedirect] = useState(false);
+
+    useEffect(() => {
+        // setAuthToken(localStorage.getItem('jwtToken'));
+        // if (localStorage.getItem('jwtToken')) {
+        setLoggedInUser(localStorage.getItem('userId'));
+        axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/timagotchis/my-timagotchis/${localStorage.getItem('userId')}`)
+            .then((response) => {
+                // data is an object
+                console.log('response', response.data);
+                setTims(response.data);
+                // let userData = jwtDecode(localStorage.getItem('jwtToken'));
+                // if (userData.email === localStorage.getItem('email')) {
+                //     setData(response.data.user[0]);
+                //     setLoading(false);
+                // } else {
+                //     router.push('/users/login');
+                // }
+
+            })
+            .catch((error) => {
+                console.log(error);
+                // router.push('/users/login');
+            });
+        // } else {
+        //     router.push('/users/login');
+        // }
+    }, [router]);
+
+
     let rows = [];
-    // Timagtochis.forEach((Timagotchi, index) => {
-    //     rows.push(<MyTimagotchi Timagotchi={Timagotchi} key={index} />);
-    // });
+    if (tims.length === 0) {
+        rows.push(<div className="justify-content-center" >
+            <br />
+            <br />
+            <h4 className='underlined'>NO TIMAGOTCHIS YET</h4>
+            <h4 className='underlined'>CLICK <Link href='/users/new-timaghotchi'>HERE</Link> TO CREATE ONE</h4>
+        </div>);
+    } else {
+        tims.forEach((Timagotchi, index) => {
+            rows.push(<MyTimagotchi Timagotchi={Timagotchi} key={index} />);
+        });
+    }
 
     return (
         <section>
@@ -14,18 +63,7 @@ export default function MyTimagotchiList({ Timagtochis }) {
             </div>
             <div className='container'>
                 <div className='row row-cols-auto'>
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
-                    <MyTimagotchi />
+                    {rows}
 
                 </div>
             </div>
