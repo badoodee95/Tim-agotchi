7b557765ead3b44959ce1c4d0777450a79c90e40
Jay Lee <Jay@Ethans-MacBook-Pro.local>
2023-08-20 18:01:24 +0900

    feat: add fadein animation and bounce animation to exclamation point

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import styles from './page.module.css';
 import MyTimagotchi from './components/MyTimagotchi';
 import { LoadingCircle } from './components/Loading';
 import Link from 'next/link';
+import 'animate.css';
 
 export default function Home() {
   const [timagotchis, setTimagotchis] = useState([]);
@@ -31,7 +32,7 @@ export default function Home() {
       <div id={styles.backgroundImage}></div>
       <section className='' id={styles.myTimagotchis}>
         <h1 className='text-center' id={styles.text}>ALL TIMAGOTCHIS</h1>
-        <div className='d-flex flex-wrap justify-content-center mt-4'>
+        <div className='animate__animated animate__fadeInUp d-flex flex-wrap justify-content-center mt-4' >
           {timagotchis.map((timagotchi) => (
             <Link href={`users/${timagotchi._id}`} key={timagotchi._id} className='text-decoration-none'>
               <MyTimagotchi Timagotchi={timagotchi} key={timagotchi._id} />
